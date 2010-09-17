cf9920a0f5c97987b0402838c33e94d05612f8a7
David Gelvin <david.gelvin@gmail.com>
2010-09-17 15:12:18 +0300

    Added license / copyright

--- managers.py
+++ managers.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+#
+#    This software is derrived from EAV-Django originally written and 
+#    copyrighted by Andrey Mikhaylenko <http://pypi.python.org/pypi/eav-django>
+#
+#    This is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This software is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with EAV-Django.  If not, see <http://gnu.org/licenses/>.
+
 from functools import wraps
 
 from django.db import models
