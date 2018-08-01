c182c515ae807ca539828784a70f25bba702b225
Iwo Herka <iwo.herka@makimo.pl>
2018-08-01 07:22:12 +0000

    Use find_packages in setuptools (#33)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name             = 'django-eav2',
@@ -8,7 +8,7 @@ setup(
     provides         = ['eav'],
     description      = 'Entity-Attribute-Value storage for Django',
     url              = 'http://github.com/makimo/django-eav2',
-    packages         = ['eav', 'tests'],
+    packages         = find_packages(),
     maintainer       = 'Iwo Herka',
     maintainer_email = 'hi@iwoherka.eu',
 
