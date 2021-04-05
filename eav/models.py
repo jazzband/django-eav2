851ca4cc74f362db491345bd3ed46648ba976ce3
Mauro <mauro@sdf.org>
2021-04-04 23:57:52 -0300

    Replaces NullBoolean with Boolean (closes: #56)

--- eav/models.py
+++ eav/models.py
@@ -381,7 +381,7 @@ class Value(models.Model):
     value_float = models.FloatField(blank = True, null = True)
     value_int   = models.IntegerField(blank = True, null = True)
     value_date  = models.DateTimeField(blank = True, null = True)
-    value_bool  = models.NullBooleanField(blank = True, null = True)
+    value_bool  = models.BooleanField(blank = True, null = True)
 
     value_enum  = models.ForeignKey(
         EnumValue,
