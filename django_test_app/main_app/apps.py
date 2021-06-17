597a5374dc3a241841750ffa1d5a13b3c6417e36
Mike <22396211+Dresdn@users.noreply.github.com>
2021-06-17 16:38:06 -0700

    tests: add new django_test_app to use for testing

---
+++ django_test_app/main_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyAppConfig(AppConfig):
+    name = 'main_app'
---
+++ django_test_app/manage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+from django.core.management import execute_from_command_line
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_test_app.settings')
+    execute_from_command_line(sys.argv)
