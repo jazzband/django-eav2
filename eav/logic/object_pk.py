3e0c5d48cb246b9e41d5b58120ac1e2cceb5aafa
Mike <22396211+Dresdn@users.noreply.github.com>
2023-10-30 08:31:05 -0700

    refactor(eav/logic): enhance primary key format determination
    - Added constants for default char field length and field mapping.
    - Reorganized imports for better clarity and readability.
    - Refactored `get_pk_format` function to leverage a predefined mapping and provide better flexibility.
    - Enhanced the documentation for the `get_pk_format` function to explain its purpose and return value.

--- eav/logic/object_pk.py
+++ eav/logic/object_pk.py
@@ -1,19 +1,45 @@
 import uuid
-
 from functools import partial
-from django.db import models
+from typing import Type
+
 from django.conf import settings
+from django.db import models
+
+#: Constants
+_DEFAULT_CHARFIELD_LEN: int = 40
+
+_FIELD_MAPPING = {
+    "django.db.models.UUIDField": partial(
+        models.UUIDField,
+        primary_key=True,
+        editable=False,
+        default=uuid.uuid4,
+    ),
+    "django.db.models.CharField": partial(
+        models.CharField,
+        primary_key=True,
+        editable=False,
+        max_length=_DEFAULT_CHARFIELD_LEN,
+    ),
+}
 
 
-def get_pk_format():
-    if settings.EAV2_PRIMARY_KEY_FIELD == "django.db.models.UUIDField":
-        PrimaryField = partial(
-            models.UUIDField, primary_key=True, editable=False, default=uuid.uuid4
-        )
-    elif settings.EAV2_PRIMARY_KEY_FIELD == "django.db.models.CharField":
-        PrimaryField = partial(
-            models.CharField, primary_key=True, editable=False, max_length=40
-        )
-    else:
-        PrimaryField = partial(models.BigAutoField, primary_key=True, editable=False)
-    return PrimaryField()
+def get_pk_format() -> Type[models.Field]:
+    """
+    Get the primary key field format based on the Django settings.
+
+    This function returns a field factory function that corresponds to the
+    primary key format specified in Django settings. If the primary key
+    format is not recognized, it defaults to using BigAutoField.
+
+    Returns:
+        Type[models.Field]: A field factory function that can be used to
+        create the primary key field instance.
+    """
+    field_factory = _FIELD_MAPPING.get(
+        settings.EAV2_PRIMARY_KEY_FIELD,
+        partial(models.BigAutoField, primary_key=True, editable=False),
+    )
+
+    # Create and return the field instance
+    return field_factory()
